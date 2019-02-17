c2e4c6eb396c22277625ac3661ba878b06723006
wingmeng <wingmeng@qq.com>
2019-02-17 19:35:48 +0800

    1. 移除 js 代码中无用的调试代码，保持代码简洁； 2. 优化 `sw.js` 中的缓存策略，只缓存本地静态文件，以防存储空间溢出。

--- sw.js
+++ sw.js
@@ -1,20 +1,19 @@
-/************\
- * 缓存资源 *
-\************/
 self.addEventListener('fetch', (event) => {
-  // console.log(caches);
   event.respondWith(
-    // new Response('hello world')
     // 从缓存中获取内容
     caches.match(event.request).then((cache) => {
-      // console.info(cache);
       // 返回匹配的缓存，如无则尝试请求
       return cache || fetch(event.request).then((res) => {
-        // console.log(res);
         // 打开缓存，如无则自动创建一个空缓存并返回
         return caches.open('restaurant-stage').then((cache) => {
-          // 添加缓存条目（存储副本）
-          cache.put(event.request, res.clone());
+          const fetchUrl = event.request.url;
+
+          // 添加缓存条目（存储副本），只缓存向本地服务器请求的资源
+          if (fetchUrl.startsWith('http://localhost')
+            || fetchUrl.startsWith('http://127.0.0.1')) {
+            cache.put(event.request, res.clone());
+          }
+
           return res;
         });
       });
